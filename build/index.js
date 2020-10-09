557f8c05ad280ccfdb740717a305a06f932af675
Rafael sanchez <eduardoxlau@hotmail.com>
2020-10-09 12:42:23 -0500

    add procces env port

--- build/index.js
+++ build/index.js
@@ -7,11 +7,12 @@ var _express = _interopRequireDefault(require("express"));
 var _routes = _interopRequireDefault(require("./routes"));
 
 var app = (0, _express["default"])();
+var port = process.env.PORT ? process.env.PORT : 3000;
 app.use("/api", _routes["default"]);
 app.get("/", function (req, res) {
   res.send("hello world");
 });
-app.listen(3000, function () {
-  return console.log("works");
+app.listen(port, function () {
+  return console.log("works in port ", port);
 });
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes";
 
 const app = express();
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use("/api", routes);
 
@@ -9,4 +10,4 @@ app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.listen(3000, () => console.log("works"));
+app.listen(port, () => console.log("works in port ", port));
