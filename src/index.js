f5bd56954e17e6c81d2e7e87474039737bb14773
Rafael sanchez <eduardoxlau@hotmail.com>
2020-10-11 16:43:49 -0500

    change port for run in dev and add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const app = express();
-const port = process.env.PORT ? process.env.PORT : 3000;
+const port = process.env.PORT ? process.env.PORT : 4000;
+
+app.use(cors());
 
 app.use("/api", routes);
 
